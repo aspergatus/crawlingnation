f917e23ccd29c22c6f31fbc0476fa236df5b6ed2
Tony Nguyen <tony20_01@hotmail.com>
2015-04-20 01:01:18 +0200

    time

--- routes/mother.js
+++ routes/mother.js
@@ -29,7 +29,7 @@ router.get('/getchildren', function(req, res) {
                      
 function refreshChildren(){
     Object.keys(childrenlist).forEach(function(key){
-        if(Math.floor(new Date() / 1000) - childrenlist[key].lastupdate > 3 * 60)
+        if(Math.floor(new Date() / 1000) - childrenlist[key].lastupdate > 3)
             delete childrenlist[key];
     });
 }
